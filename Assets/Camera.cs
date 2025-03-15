fbed72bbc39514fcc5a022ab48cc95f4d55cd12c
zhisteer <bilgee19998@gmail.com>
2025-03-15 16:04:53 +0900

    camera

--- Assets/Camera.cs
+++ Assets/Camera.cs
@@ -1,16 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class AspectRatioUtility : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Screen.SetResolution(1440, 1080, true);
+        Adjust();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Adjust()
     {
-        
+        float targetaspect = 4.0f / 3.0f;
+
+        float windowaspect = (float)Screen.width / (float)Screen.height;
+
+        float scaleheight = windowaspect / targetaspect;
+
+        Camera camera = GetComponent<Camera>();
+
+        if (scaleheight < 1.0f)
+        {
+            Rect rect = camera.rect;
+
+            rect.width = 1.0f;
+            rect.height = scaleheight;
+            rect.x = 0;
+            rect.y = (1.0f - scaleheight) / 2.0f;
+
+            camera.rect = rect;
+        }
+        else
+        {
+            float scalewidth = 1.0f / scaleheight;
+
+            Rect rect = camera.rect;
+
+            rect.width = scalewidth;
+            rect.height = 1.0f;
+            rect.x = (1.0f - scalewidth) / 2.0f;
+            rect.y = 0;
+
+            camera.rect = rect;
+        }
+
     }
 }
